64f80bb08953d39ee884830d27f1fa368ef00926
smartonapps <senskon@gmail.com>
2017-08-24 00:32:16 -0700

    do commits

--- app/src/main/java/com/amartonapps/smartonapps/MainActivity.java
+++ app/src/main/java/com/amartonapps/smartonapps/MainActivity.java
@@ -1,4 +1,4 @@
-package com.amartonapps.smartonapps;
+package com.example.csc.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // commit
+
+
+
+
     }
 }
